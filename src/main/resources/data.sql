1f5005bfa5a457089e319eb7f5ea9d0a4d5c6dc7
Sheryleen <sheryholthaus@yahoo.com>
2020-02-02 17:08:44 -0700

    Completed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,8 @@ INSERT INTO flowers (name,price,picture,rating,in_cart,on_sale) VALUES
 ,('Fresh Cut Flowers -Dendrobium Purple Orchids with Vase', 45.95,'https://images-na.ssl-images-amazon.com/images/I/71kvDYBkycL._SL1176_.jpg',5,false,false)
 ,('Sun Vista, Bouquet Wildflower Large', 13.25,'https://images-na.ssl-images-amazon.com/images/I/71%2Bb3oE8IFL._SL1024_.jpg',4,false,false)
 ,('Cut Flowers Rose 12 Stems 60Cm', 21.95,'https://images-na.ssl-images-amazon.com/images/I/41%2By8pV91YL.jpg',2,false,false)
-,('Benchmark Bouquets Light Pink Roses and White Oriental Lilies, With Vase', 44.95,'https://images-na.ssl-images-amazon.com/images/I/81qGuRXSSWL._SL1500_.jpg',4,false,true);
+,('Benchmark Bouquets Light Pink Roses and White Oriental Lilies, With Vase', 44.95,'https://images-na.ssl-images-amazon.com/images/I/81qGuRXSSWL._SL1500_.jpg',4,false,true)
+,('Stargazer Barn - Confetti Bouquet - Colorful Fresh Tulips With Vase', 32.99, 'https://images-na.ssl-images-amazon.com/images/I/71x0DpEMXtL._SL1500_.jpg', 2, false, true)
+,('Assorted Tulips Fresh Flower Double Bouquet with Purple Vase (30 Tulips)', 66.99, 'https://images-na.ssl-images-amazon.com/images/I/41azH%2BgZC-L.jpg', 4, false, false)
+,('Sunflower Succulent Bouquet (Free Vase Included)', 44.99, 'https://images-na.ssl-images-amazon.com/images/I/41sWVYxhTKL.jpg',3, false, true)
+,('Bouquet Fall Splendor', 14.00, 'https://images-na.ssl-images-amazon.com/images/I/61rJjGodbaL._AC_SL1024_.jpg',4, false, true);
