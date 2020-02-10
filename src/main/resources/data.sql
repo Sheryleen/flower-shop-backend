71fed2938578ea83cf97c85a5b88c17fe21652e3
Sheryleen <sheryholthaus@yahoo.com>
2020-02-09 17:51:25 -0700

    Final BackEnd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,26 @@
-INSERT INTO flowers (name,price,picture,rating,in_cart,on_sale) VALUES
- ('Benchmark Bouquets Flowering Fields',39.99,'https://images-na.ssl-images-amazon.com/images/I/91jap%2B1qPyL._SX679_.jpg',3,false,true)
-,('Benchmark Bouquets Signature Roses and Alstroemeria',52.50,'https://images-na.ssl-images-amazon.com/images/I/81CQM3ekF4L._SL1500_.jpg',4,false,true)
-,('Mandy 30pcs White 14" Artificial ,Fresh Cut Floral Bouquet (Vase Not Included)' ,14.95, 'https://s7img.ftdi.com/is/image/ProvideCommerce/30007596_LAY?$proflowers-hero-lv-lay$&qlt=80,0&resMode=trilin',1,false,false)
-,('Fresh Cut Flowers -Dendrobium Purple Orchids with Vase', 45.95,'https://images-na.ssl-images-amazon.com/images/I/71kvDYBkycL._SL1176_.jpg',5,false,false)
-,('Sun Vista, Bouquet Wildflower Large', 13.25,'https://images-na.ssl-images-amazon.com/images/I/71%2Bb3oE8IFL._SL1024_.jpg',4,false,false)
-,('Cut Flowers Rose 12 Stems 60Cm', 21.95,'https://images-na.ssl-images-amazon.com/images/I/41%2By8pV91YL.jpg',2,false,false)
-,('Benchmark Bouquets Light Pink Roses and White Oriental Lilies, With Vase', 44.95,'https://images-na.ssl-images-amazon.com/images/I/81qGuRXSSWL._SL1500_.jpg',4,false,true)
-,('Stargazer Barn - Confetti Bouquet - Colorful Fresh Tulips With Vase', 32.99, 'https://images-na.ssl-images-amazon.com/images/I/71x0DpEMXtL._SL1500_.jpg', 2, false, true)
-,('Assorted Tulips Fresh Flower Double Bouquet with Purple Vase (30 Tulips)', 66.99, 'https://images-na.ssl-images-amazon.com/images/I/41azH%2BgZC-L.jpg', 4, false, false)
-,('Sunflower Succulent Bouquet (Free Vase Included)', 44.99, 'https://images-na.ssl-images-amazon.com/images/I/41sWVYxhTKL.jpg',3, false, true)
-,('Bouquet Fall Splendor', 14.00, 'https://images-na.ssl-images-amazon.com/images/I/61rJjGodbaL._AC_SL1024_.jpg',4, false, true);
+INSERT INTO flowers
+    (name,price,picture,rating,in_cart,on_sale)
+VALUES
+    ('Benchmark Bouquets Flowering Fields', 39.99, 'https://images-na.ssl-images-amazon.com/images/I/91jap%2B1qPyL._SX679_.jpg', 3, false, true)
+,
+    ('Benchmark Bouquets Signature Roses and Alstroemeria', 52.50, 'https://images-na.ssl-images-amazon.com/images/I/81CQM3ekF4L._SL1500_.jpg', 4, false, true)
+,
+    ('Mandy 30pcs White 14" Artificial ,Fresh Cut Floral Bouquet (Vase Not Included)' , 14.95, 'https://s7img.ftdi.com/is/image/ProvideCommerce/30007596_LAY?$proflowers-hero-lv-lay$&qlt=80,0&resMode=trilin', 1, false, false)
+,
+    ('Fresh Cut Flowers -Dendrobium Purple Orchids with Vase', 45.95, 'https://images-na.ssl-images-amazon.com/images/I/71kvDYBkycL._SL1176_.jpg', 5, false, false)
+,
+    ('Sun Vista, Bouquet Wildflower Large', 13.25, 'https://images-na.ssl-images-amazon.com/images/I/71%2Bb3oE8IFL._SL1024_.jpg', 4, false, false)
+,
+    ('Cut Flowers Rose 12 Stems 60Cm', 21.95, 'https://images-na.ssl-images-amazon.com/images/I/41%2By8pV91YL.jpg', 2, false, false)
+,
+    ('Benchmark Bouquets Light Pink Roses and White Oriental Lilies, With Vase', 44.95, 'https://images-na.ssl-images-amazon.com/images/I/81qGuRXSSWL._SL1500_.jpg', 4, false, true)
+,
+    ('Stargazer Barn - Confetti Bouquet - Colorful Fresh Tulips With Vase', 32.99, 'https://images-na.ssl-images-amazon.com/images/I/71x0DpEMXtL._SL1500_.jpg', 2, false, true)
+,
+    ('Assorted Tulips Fresh Flower Double Bouquet with Purple Vase (30 Tulips)', 66.99, 'https://images-na.ssl-images-amazon.com/images/I/41azH%2BgZC-L.jpg', 4, false, false)
+,
+    ('Sunflower Succulent Bouquet (Free Vase Included)', 44.99, 'https://images-na.ssl-images-amazon.com/images/I/41sWVYxhTKL.jpg', 3, false, true)
+,
+    ('Bouquet Fall Splendor', 14.00, 'https://images-na.ssl-images-amazon.com/images/I/61rJjGodbaL._AC_SL1024_.jpg', 4, false, true)
+,
+    ('24 Stem Bunch of Roses', 19.99, 'https://www.amazon.com/Whole-Trade-Guarantee-Double-Colors/dp/B07N91VSZP/ref=sr_1_3?almBrandId=VUZHIFdob2xlIEZvb2Rz&fpw=alm&keywords=flowers&qid=1581295146&sr=8-3', 5, true, true);
