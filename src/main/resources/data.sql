d2d622ed27851c46da30ff08e1fd2b2e31f53a1c
Sheryleen <sheryholthaus@yahoo.com>
2020-03-07 14:47:22 -0700

    completed CapStone

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,24 +3,24 @@ INSERT INTO flowers
 VALUES
     ('Benchmark Bouquets Flowering Fields', 39.99, 'https://images-na.ssl-images-amazon.com/images/I/91jap%2B1qPyL._SX679_.jpg', 3, false, true)
 ,
-    ('Benchmark Bouquets Signature Roses and Alstroemeria', 52.25, 'https://images-na.ssl-images-amazon.com/images/I/81CQM3ekF4L._SL1500_.jpg', 4, false, true)
+    ('Signature Roses and Alstroemeria', 52.25, 'https://images-na.ssl-images-amazon.com/images/I/81CQM3ekF4L._SL1500_.jpg', 4, false, true)
 ,
-    ('Mandy 30pcs White 14" Artificial ,Fresh Cut Floral Bouquet (Vase Not Included)' , 14.95, 'https://s7img.ftdi.com/is/image/ProvideCommerce/30007596_LAY?$proflowers-hero-lv-lay$&qlt=80,0&resMode=trilin', 1, false, false)
+    ('Fresh Cut Floral Bouquet' , 14.95, 'https://s7img.ftdi.com/is/image/ProvideCommerce/30007596_LAY?$proflowers-hero-lv-lay$&qlt=80,0&resMode=trilin', 1, false, false)
 ,
-    ('Fresh Cut Flowers -Dendrobium Purple Orchids with Vase', 45.95, 'https://images-na.ssl-images-amazon.com/images/I/71kvDYBkycL._SL1176_.jpg', 5, false, false)
+    ('Dendrobium Purple Orchids', 45.95, 'https://images-na.ssl-images-amazon.com/images/I/71kvDYBkycL._SL1176_.jpg', 5, false, false)
 ,
     ('Sun Vista, Bouquet Wildflower Large', 13.25, 'https://images-na.ssl-images-amazon.com/images/I/71%2Bb3oE8IFL._SL1024_.jpg', 4, false, false)
 ,
     ('Cut Flowers Rose 12 Stems 60Cm', 21.95, 'https://images-na.ssl-images-amazon.com/images/I/41%2By8pV91YL.jpg', 2, false, false)
 ,
-    ('Benchmark Bouquets Light Pink Roses and White Oriental Lilies, With Vase', 44.95, 'https://images-na.ssl-images-amazon.com/images/I/81qGuRXSSWL._SL1500_.jpg', 4, false, true)
+    ('Light Pink Roses and White Oriental Lilies', 44.95, 'https://images-na.ssl-images-amazon.com/images/I/81qGuRXSSWL._SL1500_.jpg', 4, false, true)
 ,
-    ('Stargazer Barn - Confetti Bouquet - Colorful Fresh Tulips With Vase', 32.99, 'https://images-na.ssl-images-amazon.com/images/I/71x0DpEMXtL._SL1500_.jpg', 2, false, true)
+    ('Colorful Fresh Tulips', 32.99, 'https://images-na.ssl-images-amazon.com/images/I/71x0DpEMXtL._SL1500_.jpg', 2, false, true)
 ,
-    ('Assorted Tulips Fresh Flower Double Bouquet with Purple Vase (30 Tulips)', 66.99, 'https://images-na.ssl-images-amazon.com/images/I/41azH%2BgZC-L.jpg', 4, false, false)
+    ('Assorted Tulips Double Bouquet with Purple Vase (30 Tulips)', 66.99, 'https://images-na.ssl-images-amazon.com/images/I/41azH%2BgZC-L.jpg', 4, false, false)
 ,
     ('Sunflower Succulent Bouquet (Free Vase Included)', 44.99, 'https://images-na.ssl-images-amazon.com/images/I/41sWVYxhTKL.jpg', 3, false, true)
 ,
     ('Bouquet Fall Splendor', 14.95, 'https://images-na.ssl-images-amazon.com/images/I/61rJjGodbaL._AC_SL1024_.jpg', 4, false, true)
 ,
-    ('24 Stem Bunch of Roses', 19.99, 'https://images-na.ssl-images-amazon.com/images/I/711YkUI81PL._AC_SL1500_.jpg', 5, true, true);
+    ('24 Stem Bunch of Roses', 19.99, 'https://images-na.ssl-images-amazon.com/images/I/711YkUI81PL._AC_SL1500_.jpg', 5, false, true);
